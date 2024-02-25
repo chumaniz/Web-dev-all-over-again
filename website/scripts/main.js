2350692f589e8971037d8c75fd87e5422191ff23
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-25 11:44:05 +0200

    Added alerts to js script

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,2 +1,16 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Koenigsegg";
+
+let myMessage = "Hello there";
+myMessage = "There has been a change in plans";
+/*
+Apparently I am able to assign a value to a vaeriable
+and change it as many times as I want
+*/
+
+let iceCream = "Strawberry";
+if (iceCream === "Banana"){
+    alert ("Oh man oh man oh man!")
+}else{
+    alert("Awww but banana is my favourite!")
+}
