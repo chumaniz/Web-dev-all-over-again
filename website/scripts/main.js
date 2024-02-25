41498cbaa1f1a61304a56182eeaff75c3be5d9ad
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-25 18:13:01 +0200

    Added image changer using JavaScript

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -3,6 +3,17 @@ myHeading.textContent = "Koenigsegg";
 
 alert("Hello!");
 
-document.querySelector("html").addEventListener("click", function() {
-    alert("Ow stop poking me!");
-});
+// document.querySelector("html").addEventListener("click", function() {
+//     alert("Ow stop poking me!");
+// });
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+   const mySrc = myImage.getAttribute("src");
+   if (mySrc === "../images/koenigsegg.jpg"){
+    myImage.setAttribute("src", "../images/koenigsegg2.jpg");
+   } else {
+    myImage.setAttribute("src", "../images/koenigsegg.jpg");
+   }
+};
