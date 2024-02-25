17796026ea2c1d3b7d2eb8642d57635817c55258
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-25 21:14:34 +0200

    Added personalized welcome message in JS

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,19 +1,37 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Koenigsegg";
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Koenigsegg";
 
-alert("Hello!");
+// alert("Hello!");
 
 // document.querySelector("html").addEventListener("click", function() {
 //     alert("Ow stop poking me!");
 // });
 
-const myImage = document.querySelector("img");
+// const myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-   const mySrc = myImage.getAttribute("src");
-   if (mySrc === "../images/koenigsegg.jpg"){
-    myImage.setAttribute("src", "../images/koenigsegg2.jpg");
-   } else {
-    myImage.setAttribute("src", "../images/koenigsegg.jpg");
-   }
-};
+// myImage.onclick = () => {
+//    const mySrc = myImage.getAttribute("src");
+//    if (mySrc === "../images/koenigsegg.jpg"){
+//     myImage.setAttribute("src", "../images/koenigsegg2.jpg");
+//    } else {
+//     myImage.setAttribute("src", "../images/koenigsegg.jpg");
+//    }
+// };
+
+let myButton = document.querySelector("button");
+myButton.onclick = () => {
+    setUserName();
+}
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Koenigsegg...where speed meets comfort. Welcome, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+}   else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Koenigsegg...where speed meets comofrt. Welcome, ${myName}`;
+}
